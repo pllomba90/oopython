a8df577c967b30c69b9e672ca9574c12a95c0958
Peter Lombardo <peterllombardo@gmail.com>
2023-04-06 17:53:10 -0400

    Final product

--- serial.py
+++ serial.py
@@ -19,3 +19,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__ (self,start=0):
+        """Initial creation of a serial number starting at 0 unless otherwise indicated."""
+        self.start = self.next = start
+
+    def __repr__ (self):
+        """Creates a representation of the class"""
+        return f"<SerialGenerator start={self.start} next={self.next}>"
+    
+    def generate(self):
+        """Method to actually make a serial number"""
+        self.next += 1
+        return self.next - 1
+    
+    def reset(self):
+        """Resets our generator."""
+        self.next = self.start
